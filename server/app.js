7ba41c11acedd673e890adee7346b67c29849ea9
Priambodo-Kurniawan <priambodo.2009@gmail.com>
2020-02-27 14:15:49 +0700

    refactor route backend api

--- server/app.js
+++ server/app.js
@@ -2,17 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const router = require('./routes/index');
 
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-
-var todos = require('./routes/todos');
-app.use('/api/todos', todos);
-
-var users = require('./routes/users');
-app.use('/api/users', users);
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
 const router  = Router();
+const todoRouter = require('./routes/todos');
+const userRouter = require('./routes/users');
 
+router.use('/api/todos', todoRouter);
+router.use('/api/todos', userRouter);
 router.get('/', (req, res) => res.send('Welcome'));
 
 module.exports = router;
